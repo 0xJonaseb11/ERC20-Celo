090067e46b0b196941699d31f8c47dce3ff24701
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 01:58:18 +0200

    update

--- PepeToken.sol
+++ PepeToken.sol
@@ -5,11 +5,7 @@ import { ERC20 } from "@Openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PepeToken is ERC20, Ownable {
-    constructor() ERC20("PepeToken", "Pepe") {
-
-
-
-    }
+    constructor() ERC20("PepeToken", "Pepe") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
