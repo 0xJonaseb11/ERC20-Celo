47ddafd79085035aaa4e68cc418539e0268476ea
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 02:42:09 +0200

    update key

--- contracts/Jazoom.sol
+++ contracts/Jazoom.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.19;
 import { ERC20 } from "@Openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PepeToken is ERC20, Ownable {
-    constructor() ERC20("PepeToken", "Pepe") {}
+contract Jazoom is ERC20, Ownable {
+    constructor() ERC20("Jazoom", "jaz") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
