ba6c35ef2d4cb521f907540db5b66bcfad95f799
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 02:10:01 +0200

    update deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,2 +1,23 @@
 const { ethers } = require("hardhat";
 const dotenv = dotenv.config();
+
+const main = async () =>{
+
+  const PepeToken = await ethers.getContractFactory("PepeToken");
+  const pepToken = await PepeToken.deploy();
+
+  console.log("PepeToken Deployed to:",pepToken.address);
+
+}
+
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error("Error deploying Contract:", error);
+    process.exit(1);
+  }
+}
+
+runMain();
