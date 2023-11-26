bcc5b54d53e2116facd2e0badf8276fee1e7d2c0
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 02:17:57 +0200

    update deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,13 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+
+  alfajores: {
+    url: "https://alfajores-forno.celo-testnet.org",
+    accounts: {
+      mnemonic: process.env.MNEMONIC,
+      path: "m/44'/52752'/0'/0"
+    },
+    chainId: 44787
+  }
 };
